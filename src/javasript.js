688e34b32e99e58be596da8313ee5c707f5b8fc7
THIAGO OLIVEIRA FRANCA <tfranca@ufba.br>
2024-11-22 11:08:57 -0300

    Update javasript.js

--- src/javasript.js
+++ src/javasript.js
@@ -1,110 +1,160 @@
-let num = "";
-let lista = [];
-qnt_multdiv = 0;
-qnt_simples = 0; //adicao e subtracao
+class Calculadora {
+    constructor() {
+        this.num = '';
 
-
-function adicionar_ao_display(conta) {
-    var mostra = document.getElementById("tela")
-    mostra.textContent = conta;
-
-}
-
-function concatenar_numero(numero) {
-    num += numero;
-    adicionar_ao_display(num)
-
-}
-
-function operacao(operando)  {
-    if (operando === '*'|| operando === "/") {
-        qnt_multdiv += 1;
-    }
-    else if (operando === '+' || operando === '-') {
-        qnt_simples += 1;
-    }
-    let numero_convertido = Number(num);
-    lista.push(numero_convertido);
-    num = "";
-    if (operando !== '=' ) {
-        lista.push(operando);
-    }
-    
-    adicionar_ao_display(operando)
-
-    if (operando === "=") {
-        if (qnt_multdiv > 0) {
-            while(qnt_multdiv > 0) {
-                verificar_multdiv(lista)
-            }
-        }
-        if (qnt_simples > 0) {
-            while(qnt_simples > 0) {
-                resolver_simples(lista)
-            }
-        }
-
-        adicionar_ao_display(lista[0])
-        num = lista[0]
-        lista = []
-        qnt_multdiv = 0;
-        qnt_simples = 0;        
-    }
+        const calculo = new Calculo();
+        this.calculo = calculo;
         
+        const display = new Display();
+        this.display = display;
     }
 
-function verificar_multdiv(lista) {
-        for (let i=1; i<lista.length-1; i += 2) {
-            if (lista[i] === "*") {
-                lista[i-1] = lista[i-1] * lista[i+1];
-                lista.splice(i,2);
-                qnt_multdiv -= 1;
-                return
+    concatenarNumero(numero) {
+        this.num += numero;
+        this.display.adicionarAoDisplay(this.num)
+    }
+
+    limparNum () {
+        this.num = "";
+    }
+
+    lerOperando(operando) {
+        if (operando === '*'|| operando === "/") {
+            this.calculo.incrementarMultDiv();
+        }
+        else if (operando === '+' || operando === '-') {
+            this.calculo.incrementarSomasSub();
+        }
+    }
+
+    operacao(operando) {
+        this.lerOperando(operando);
+        if (operando === "=" && this.calculo.lista.length === 0) {
+            this.display.adicionarAoDisplay("Erro");
+            return;
+        }
+        let numero_convertido = Number(this.num);
+        
+        this.calculo.lista.push(numero_convertido);
+        this.limparNum();
+        
+        if (operando !== '=' ) {
+            this.calculo.lista.push(operando);
+        }
+        
+        this.display.adicionarAoDisplay(operando)
+    
+        if (operando === "=") {
+            if (this.calculo.qnt_multdiv > 0) {
+                while(this.calculo.qnt_multdiv > 0) {
+                    this.calculo.resolverMultDiv(this.calculo.lista)
+                }
             }
-            else if(lista[i] === "/") {
-                lista[i-1] = lista[i-1] / lista[i+1];
-                lista.splice(i,2);
-                qnt_multdiv -= 1;
-                return
+            if (this.calculo.qnt_simples > 0) {
+                while(this.calculo.qnt_simples > 0) {
+                    this.calculo.resolverSomasSub(this.calculo.lista)
+                }
             }
-        }   
+    
+            this.display.adicionarAoDisplay(this.calculo.lista[0])
+            this.calculo.zerarConta();
+        }        
+    }    
+}
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    if (!isNaN(key)) {
+        calculadora.concatenarNumero(key); // Para números
+    }
+    else if (key === '+' || key === '-' || key === '*' || key === '/') {
+        calculadora.operacao(key); // Para operadores
+    }
+    else if (key === 'Enter') {
+        calculadora.operacao('='); // Para igual
+    }
+});
+
+class Calculo {
+    constructor() {
+        this.qnt_multdiv = 0; //multiplicacao e divisao
+        this.qnt_simples = 0; //adicao e subtracao
+        this.lista = [];
     }
     
-    function resolver_simples (lista) {
+    incrementarMultDiv () {
+        this.qnt_multdiv += 1;
+    }
+    incrementarSomasSub() {
+        this.qnt_simples += 1;
+    }
+
+    resetar() {
+        this.lista = [];
+        this.qnt_multdiv = 0;
+        this.qnt_simples = 0;
+        this.num = "";
+        calculadora.display.adicionarAoDisplay("");
+    }
+
+    resolverSomasSub (lista) {
         for (let i=1; i<lista.length-1; i += 2) {
             if (lista[i] === "+") {
                 lista[i-1] += lista[i+1]
                 lista.splice(i,2)
-                qnt_simples -= 1;
+                this.qnt_simples -= 1;
                 return
             }
             else if(lista[i] === '-') {
                 lista[i-1] -= lista[i+1]
                 lista.splice(i,2)
-                qnt_simples -= 1;
+                this.qnt_simples -= 1;
                 return
             }
         }
     }
-    
-function resetar() {
-    lista = []
-    qnt_multdiv = 0;
-    qnt_simples = 0;
-    num = ""
-    adicionar_ao_display("")
+
+    resolverMultDiv(lista) {
+        for (let i=1; i<lista.length-1; i += 2) {
+            if (lista[i] === "*") {
+                lista[i-1] = lista[i-1] * lista[i+1];
+                lista.splice(i,2);
+                this.qnt_multdiv -= 1;
+                return
+            }
+            else if(lista[i] === "/" && lista[i+1] === 0) {
+                    this.lista[0] = 0;
+                    this.zerarConta()
+                    return;
+            }
+            else if (lista[i] === "/") {
+                lista[i-1] = lista[i-1] / lista[i+1];
+                lista.splice(i,2);
+                this.qnt_multdiv -= 1;
+                return
+            }
+        }
+        return null;   
+    }
+
+    zerarConta() {
+        calculadora.num = this.lista[0]
+        this.lista = []
+        this.qnt_multdiv = 0;
+        this.qnt_simples = 0;        
+    }
 
 }
 
+class Display {
+    constructor() {
 
-document.addEventListener('keydown', function(event) {
-    const key = event.key;
-    // Verifica se a tecla pressionada é um número ou um operador
-    if (!isNaN(key)) {
-        concatenar_numero(key); // Para números
-    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
-        operacao(key); // Para operadores
-    } else if (key === 'Enter') {
-        operacao('='); // Para igual
     }
-});
+    adicionarAoDisplay(conta) {
+        var mostra = document.getElementById("tela")
+        mostra.textContent = conta;
+    }
+}
+
+
+const calculadora = new Calculadora();
